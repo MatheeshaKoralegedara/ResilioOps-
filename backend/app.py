04934a2c16bf9d43b9795ff51d142b0db251ff85
Matheesha Koralegedara <matheeshakorale19@gmail.com>
2026-05-27 15:36:39 +0530

    Update Backend code

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,53 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
+import psycopg2
+import redis
 
 app = Flask(__name__)
 
+# PostgreSQL connection
+def get_db():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD")
+    )
+
+# Redis connection
+cache = redis.Redis(
+    host=os.getenv("REDIS_HOST"),
+    port=6379,
+    decode_responses=True
+)
+
 @app.route("/")
 def home():
-    return {
-        "message": "ResilioOps Backend Running",
-        "database": os.getenv("DB_HOST"),
-        "redis": os.getenv("REDIS_HOST")
-    }
+    return jsonify({
+        "message": "ResilioOps Backend Connected",
+        "status": "running"
+    })
 
-@app.route("/health")
-def health():
-    return {
-        "status": "healthy"
-    }
+@app.route("/db-test")
+def db_test():
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+    version = cur.fetchone()
+    conn.close()
+
+    return jsonify({
+        "postgres_version": version[0]
+    })
+
+@app.route("/cache-test")
+def cache_test():
+    cache.set("devops", "resilioops-running")
+    value = cache.get("devops")
+
+    return jsonify({
+        "redis_value": value
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
